371bd1ad2bd3aa5cdba1c6da7324c018f2b892f3
Matt Riley <matt.riley1878@gmail.com>
2017-04-26 11:48:29 +0100

    Updated schema

---
+++ src/schema/index.js
@@ -0,0 +1,31 @@
+import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
+import Mocks from '../../test/mocks';
+import Resolvers from '../resolvers';
+
+import RootQuery from './root.graphql';
+
+const logger = { log: err => console.log(err) };
+
+const SchemaDefinition = `
+  schema {
+    query: RootQueryType
+  }`;
+
+const Schema = [
+  RootQuery,
+];
+
+const executableSchema = makeExecutableSchema({
+  typeDefs: Schema,
+  resolvers: Resolvers,
+  allowUndefinedInResolve: true,
+  logger
+});
+
+addMockFunctionsToSchema({
+  schema: executableSchema,
+  mocks: Mocks,
+  preserveResolvers: true,
+});
+
+export default executableSchema;
